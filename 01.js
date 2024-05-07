60b5d098c0f808c50a72b424921f88a0ded9b913
JoYoungJinNode5 <jyj950406@naver.com>
2024-05-07 09:43:49 +0900

    23

---
+++ 01.js
@@ -0,0 +1,8 @@
+function solution(num1, num2) {
+    return num1- num2;
+};
+
+var num1 = 2;
+var num2 = 3;
+
+console.log(solution(num1, num2))
---
+++ 19.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    // return Number.isInteger(Math.sqrt(n)) ? Math.pow(Math.sqrt(n) + 1,2) : -1
+    var answer = 0;
+    if ( Number.isInteger(n) = true) {
+        if (Math.sqrt(n)) {
+            if (Number.isInteger(n) = true) {
+                answer = (Math.sqrt(n) + 1) ** 2
+            } else {
+                answer = -1
+            }
+        }
+    }
+    return answer;
+}
+
+var num = 36;
+console.log(solution(num));
