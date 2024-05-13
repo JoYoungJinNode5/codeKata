c38ea0c39f29d65afa8de1bf0f548379e9d0b232
JoYoungJinNode5 <jyj950406@naver.com>
2024-05-13 09:40:50 +0900

    29

--- 29.js
+++ 29.js
@@ -1,8 +1,10 @@
 function solution(arr) {
   var answer = [];
+  // 1. 배열 길이가 1이하일 때 -1을 반환하는 함수 생성
   if (arr.length <= 1) {
     return [-1];
   } else {
+    // 2. 배열에서 가장 작은 수를 indexOf로 찾아내 splice로 '하나' 도려냄
     arr.splice(arr.indexOf(Math.min(...arr)), 1);
     answer = arr;
   }
